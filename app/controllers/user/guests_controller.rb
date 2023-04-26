ff87f5e2495125f9de61e4e00ea31669166b3ea4
EC2 Default User <ec2-user@ip-172-31-7-212.ap-northeast-1.compute.internal>
2023-04-26 10:57:59 +0000

    png

--- app/controllers/user/guests_controller.rb
+++ app/controllers/user/guests_controller.rb
@@ -5,7 +5,7 @@ class User::GuestsController < ApplicationController
    customer = Customer.find_or_create_by!(user_name:'guest', account_name:'guest', email: 'guest@example.com') do |customer|
      customer.password = SecureRandom.urlsafe_base64
      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
-    customer.account_image_url.attach(io: default_guest_image, filename: 'default-image.jpg', content_type: 'image/jpeg')
+    customer.account_image_url.attach(io: default_guest_image, filename: 'default-image.png', content_type: 'image/png')
       customer.save!
       #ゲストイメージ
    end
